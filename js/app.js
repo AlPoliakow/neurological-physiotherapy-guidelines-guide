4fb966987fdb2c4c4562e8a84d72a710fb332ddd
Al_P <al.e.poliakow@gmail.com>
2024-03-14 10:51:44 +1100

    Droplist JavaScript + partial content CSS

--- js/app.js
+++ js/app.js
@@ -1,28 +1,29 @@
-const condition = document.querySelector("#condition");
+const conditionList = document.querySelector("#condition");
 const country = document.querySelector("country");
 const result = document.querySelector(".result");
 
-condition.addEventListener("change", function (e) {
-  selectedCondition = e.target.value;
-  console.log(selectedCondition);
+//e.options[e.selectedIndex].value;
+
+conditionList.addEventListener("change", function (e) {
+  condition = e.target.value;
+  console.log(condition);
   updateResultsDiv();
 });
 
+
 const updateResultsDiv = function () {
-  result.innerHTML = `This is where the links to the relevant guidelines will be displayed.`;
-  if (option === "sci") {
-        result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian SCI Clinical Practice Guidelines 2023</a>`;
-    } else if (condition === "ms") {
-        result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian MS Clinical Practice Guidelines *YEAR*</a>`;
-  } else if (condition === "cp") {
+  if (condition === "sci") {
+    result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian SCI Clinical Practice Guidelines 2023</a>`;
+    console.log("SCI selected");
+  } else if (condition === "ms") {
     result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian MS Clinical Practice Guidelines *YEAR*</a>`;
+    console.log("MS selected");
+  } else if (condition === "cp") {
+    result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian CP Clinical Practice Guidelines *YEAR*</a>`;
+    console.log("CP selected");
   } else if (condition === "cva") {
-    if (country === "Australia"){
-        result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian MS Clinical Practice Guidelines *YEAR*</a>`;
-    }
-    if (country !== "Australia"){
-        result.innerText = `Link to be added`;
-    }
+    result.innerHTML = `<a href="" target="_blank" rel="noopener noreferer">Australian CVA Clinical Practice Guidelines *YEAR*</a>`;
+    console.log("CVA selected");
   }
 };
 
